bc65afeab319836d07eb475bf114d54ee01d368c
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2023-05-01 17:38:44 +0530

    first commit

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        require: true,
+        min: 3,
+        max: 20,
+        Unique: true,
+    },
+    email:{
+        type: String,
+        require: true,
+        max: 50,
+        Unique: true,
+    },
+    password:{
+        type: String,
+        require: true,
+        min: 6,
+    },
+},
+    { timestamps:true }
+);
+
+module.exports = mongoose.model("User", UserSchema)
